bfbf00b035c2834586180f93761e36c120b26ab8
JennaKoslowski <jenna.koslowski@yahoo.com>
2019-03-14 20:42:40 -0500

    initial commit- template for blog

---
+++ pages/blog/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="blog">
+        <article class="post">
+        <div> </div>
+        <h1>Building in Nuxt.js</h1>
+        <p>This is my first try with nuxt.js and vue. Let's see how it goes.</p>
+        </article>
+        <article class="post">
+        <div> </div>
+        <h1>Getting a project in GitHub</h1>
+        <p>Here I'm showing an easy way to add your current project to GitHub.</p>
+        </article>
+    </section>
+</template>
+
+<style scoped>
+    .blog {
+
+    }
+    .post {
+        background: gray;
+
+    }
+</style>
