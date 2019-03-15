cb21ccb4ea7f4609d9e05e443bbe1a567e80cb4e
JennaKoslowski <jenna.koslowski@yahoo.com>
2019-03-15 07:02:31 -0500

    Enter information on about me page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,2 +1,8 @@
 <template>
+<section>
+    <h1>About Me:</h1>
+    <p> I am a front-end web developer who focuses on JavaScript and Vue with experience with React.
+        I was a recipient of the Grow With Google Scholarship in 2018 and was chosen to receive the full nanodegree
+        which I completed in November of 2018. Since then I have been looking deeper into CSS and other JavaScript libraries and frameworks.</p> 
+</section>
 </template>
