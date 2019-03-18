826e59716521838c7554f4c91996e1a704ca5c46
JennaKoslowski <jenna.koslowski@yahoo.com>
2019-03-17 19:22:59 -0500

    Posts component VUE Component template should contain exactly one root element error

---
+++ components/Posts.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+  <article class="posts"> 
+    <h1>{{ title }}</h1>
+    <p>{{ previewText }}</p>
+  </article>
+</div>
+</template>
+
+<script>
+    export default {
+        props: ['title', 'previewText']
+    }
+</script>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,17 +1,24 @@
 <template>
+
     <section class="blog">
-        <article class="post">
-        <div> </div>
-        <h1>Building in Nuxt.js</h1>
-        <p>This is my first try with nuxt.js and vue. Let's see how it goes.</p>
-        </article>
-        <article class="post">
-        <div> </div>
-        <h1>Getting a project in GitHub</h1>
-        <p>Here I'm showing an easy way to add your current project to GitHub.</p>
-        </article>
+
+        <Posts 
+            title="Building with Nuxt" 
+            previewText="This is my first try with nuxt.js and vue. Let's see how it goes." />
+
     </section>
+
 </template>
+
+<script>
+    import Posts from '@/components/Posts'
+
+    export default {
+        components: {
+            Posts
+        }
+    }
+</script>
 
 <style scoped>
     .blog {
