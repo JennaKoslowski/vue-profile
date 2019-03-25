26575f74adeac818e9cee795c5c955b7ca49a0aa
JennaKoslowski <jenna.koslowski@yahoo.com>
2019-03-25 07:51:31 -0500

    begin styling nav bar- looks horrible

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,20 +2,30 @@
 <div class="container">
     <nav class="navbar">
         <nuxt-link class="navbar-item" to="/">Home</nuxt-link>
-          <nuxt-link class="navbar-item" to="/about">About</nuxt-link>
-          <nuxt-link class="navbar-item" to="/blog">Blog</nuxt-link>
-          <nuxt-link class="navbar-item" to="/projects">Projects</nuxt-link>
-          <nuxt-link class="navbar-item" to="/contact">Contact</nuxt-link>
+        <nuxt-link class="navbar-item" to="/about">About</nuxt-link>
+        <nuxt-link class="navbar-item" to="/blog">Blog</nuxt-link>
+        <nuxt-link class="navbar-item" to="/projects">Projects</nuxt-link>
+        <nuxt-link class="navbar-item" to="/contact">Contact</nuxt-link>
     </nav>
 </div>
 </template>
 
-<script>
-import NavBar from '../comoponents/NavBar'
 
-export default {
-    components: {
-        NavBar
+<style scoped>
+    .container {
+      display: flex;
+      justify-content: flex-end;
+      height: 100vh;
+      width: 10vw;
     }
-}
-</script>
+  .navbar {
+      background-color: #ad70e0;
+      color: black;
+      text-decoration: underline; 
+      height: 100vh;
+      width: 10vw;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+  }
+</style>
