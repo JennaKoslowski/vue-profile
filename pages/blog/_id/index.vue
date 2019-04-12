21f026504a55543cd615b6388920ec9934be60ff
JennaKoslowski <jenna.koslowski@yahoo.com>
2019-04-12 07:21:41 -0500

    remove "follow me" section from blog post templates

--- pages/blog/_id/index.vue
+++ pages/blog/_id/index.vue
@@ -3,11 +3,6 @@
         <h1>{{ $route.params.id}}</h1>
         <h3> By: Jenna Koslowski</h3>
         <p>Blog article here.</p>
-        <p>Follow me on my social media: 
-            <a href="https://github.com/JennaKoslowski">GitHub</a>, 
-        <a href="https://www.linkedin.com/in/jennakoslowski/">LinkedIn</a>, 
-        <a href="https://twitter.com/JennaKoslowski">Twitter</a>, and
-        <a href="https://www.instagram.com/jenna.koslowski/">Instagram</a>
         </p>
     </section>
 </template>
