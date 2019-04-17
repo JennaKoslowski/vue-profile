e4cc3cf6af593c4d5b230fb8a6af3b309179cc42
JennaKoslowski <jenna.koslowski@yahoo.com>
2019-04-17 17:20:53 -0500

    add title

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,6 +1,18 @@
 <template>
-
+    <h1 class= "title">
+        Projects
+    </h1>
 </template>
 
 <style scoped>
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  text-align: center;
+  font-weight: 300;
+  font-size: 100px;
+  color: #4c535e;
+  letter-spacing: 1px;
+}
 </style>
