4449e36d7bc85fe293b9d2ce4f87295e0909f1c3
JennaKoslowski <jenna.koslowski@yahoo.com>
2019-04-20 16:06:44 -0500

    edit project order

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -6,27 +6,27 @@
     <div class="projectblock">
     <h2> Current Projects </h2>
     <section class="currentprojects">
-            <Projects class="projects"
+        <Projects class="projects"
             title="Coffee Map"
             languages= "HTML, CSS, JS, ReactJS"
             github= 'https://github.com/JennaKoslowski/neighborhood-maps' />
-            <Projects class="projects"
+        <Projects class="projects"
             title="Vue Profile"
             languages= "HTML, CSS, JS, VueJS"
-            github= "https://github.com/JennaKoslowski/vue-profile" />
+            github= "https://github.com/JennaKoslowski/vue-profile" />   
     </section>
     <h2>Completed Projects</h2>
     <section class="completedprojects"> 
-        <Projects class="projects"
-            title="Arcade Game"
-            languages= "HTML, CSS, JS"
-            screenshot="../../arcadeGame.JPG"
-            github="https://github.com/JennaKoslowski/frontend-nanodegree-arcade-game" />
         <Projects class="projects"
             title="MyReads"
             languages= "HTML, CSS, JS, ReactJS"
             screenshot="../../myReads.JPG"
             github= "https://github.com/JennaKoslowski/react-myreads" />
+        <Projects class="projects"
+            title="Arcade Game"
+            languages= "HTML, CSS, JS"
+            screenshot="../../arcadeGame.JPG"
+            github="https://github.com/JennaKoslowski/frontend-nanodegree-arcade-game" />
         <Projects class="projects"
             title="Memory Game"
             languages= "HTML, CSS, JS"
