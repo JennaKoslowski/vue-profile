2f29fe77e80e396b054c0d7b6eafe5c0b6dfae53
JennaKoslowski <jenna.koslowski@yahoo.com>
2019-06-26 07:08:52 -0500

    add storyblock/ asyncData

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,23 +2,11 @@
 
     <section class="blog">
         <h1 class= "title">
-        Blog
-    </h1>
-        <Posts class="post" id="5"
-            title="Creating with Vue.js and Nuxt.js: The static sidebars"
-            previewText="The past week or so I started to create the sidebars for my profile. Right now I'm mainly working on getting all the information done and looking semi-decent. " />
-        <Posts class="post" id="4"
-            title="Creating with Vue.js and Nuxt.js: Initial reaction"
-            previewText="When I decided to create a personal portfolio website I spent probably more time than I needed to watching YouTube videos to decide whether I should attempt React, Angular or Vue." />
-        <Posts class="post" id="3"
-            title="My Udacity Nanodegree story"
-            previewText="Basically this wasn’t a snap second decision. I knew this was a new(ish) thing I wanted to learn and pursue." />   
-        <Posts class="post" id="2"
-            title="So you want to learn how to code" 
-            previewText="I did go through one program completely there are plenty of other resources I used to supplement that learning as well as programs I went through before and some I plan to do coming up." />
-        <Posts class="post" id="1"
-            title="Phase one recap: Google developer challenge scholarship" 
-            previewText="I was accepted into phase two of the Google Developer Challenge Scholarship so I decided to talk about how I got into phase two and some tips for people who are learning coding." />
+            Blog
+        </h1>
+        <nuxt-link v-for="post in posts"
+            :key="post.id"
+            :to="post.id"></nuxt-link>
     </section>
 
 </template>
@@ -30,6 +18,14 @@
         components: {
             Posts
         },
+        asycData(){
+            return context.app.$storyapi.get('cdn/stories/blog', {
+                version: 'draft'
+            }).then(res=>{
+                console.log(res);
+                return res;
+            });
+        }
     }
 </script>
 
